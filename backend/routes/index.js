f573892c8f6e8bf13ad4da4c4f7c3dbe87a7fa6e
Đinh Khắc Hoàng <dinhkhachoang7a@gmail.com>
2026-06-20 18:30:04 +0700

    feat: add activity log model and helper

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import authRoutes from "./auth.js";
+import userRoutes from "./user.js";
 import workspaceRoutes from "./workspace.js";
 import projectRoutes from "./project.js";
 import taskRoutes from "./task.js";
@@ -8,6 +9,7 @@ import taskRoutes from "./task.js";
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
 router.use("/workspaces", workspaceRoutes);
 router.use("/projects", projectRoutes);
 router.use("/tasks", taskRoutes);
